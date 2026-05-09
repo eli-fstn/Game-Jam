16314fd320678d9d1d5117a90de19ab6ee1ffbb5
acid-rock <138583992+acid-rock@users.noreply.github.com>
2026-05-09 19:01:36 +0800

    feat: added basic attack and dummy enemy

--- src/player.gd
+++ src/player.gd
@@ -6,11 +6,32 @@ var hitbox_offset: Vector2
 
 @onready var sprite = $AnimatedSprite2D
 @onready var footsteps = $AudioStreamPlayer2D
+@export var hitbox: Area2D
+
+var is_attacking = false
+
+func _ready():
+	hitbox_offset = hitbox.position
 
 func _physics_process(_delta) -> void:
+	hitbox.monitoring = false
+	
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		attack()
+		
+	if is_attacking:
+		velocity = Vector2.ZERO
+		return
+	
 	process_movement()
 	process_animation()
 	move_and_slide()
+	
+func attack():
+	is_attacking = true
+	hitbox.monitoring = true
+	print("attack")
+	play_animation("attack", last_direction)
 
 func process_movement() -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -18,10 +39,14 @@ func process_movement() -> void:
 	if direction != Vector2.ZERO:
 		velocity = direction * speed
 		last_direction = direction
+		update_hitbox_offset()
 	else:
 		velocity = Vector2.ZERO
 	
 func process_animation() -> void:
+	if is_attacking:
+		return
+		
 	if velocity != Vector2.ZERO:
 		if not footsteps.playing:
 			footsteps.play()
@@ -41,3 +66,29 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		else:
 			sprite.play(prefix + "_back")
 			
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if is_attacking:
+		is_attacking = false
+	
+func update_hitbox_offset():
+	var x := hitbox_offset.x
+	var y := hitbox_offset.y
+	
+	match last_direction:
+		Vector2.UP:
+			hitbox.position = Vector2(x, -y)
+			hitbox.rotation_degrees = 0
+		Vector2.DOWN:
+			hitbox.position = Vector2(x, y)
+			hitbox.rotation_degrees = 0
+		Vector2.RIGHT:
+			hitbox.position = Vector2(y, -x)
+			hitbox.rotation_degrees = 90
+		Vector2.LEFT:
+			hitbox.position = Vector2(-y, x)
+			hitbox.rotation_degrees = 90
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if is_attacking:
+		print("Hit")
