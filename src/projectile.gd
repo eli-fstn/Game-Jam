bfb8ae6f67de286ecf17995448fabfaa557f1173
acid-rock <lorenztuboro00@gmail.com>
2026-05-16 04:02:39 +0800

    feat: added collision to enemy

--- src/projectile.gd
+++ src/projectile.gd
@@ -9,3 +9,11 @@ func _ready():
 
 func _physics_process(delta):
 	position += direction * speed * delta
+
+func _on_body_entered(body):
+	if body.has_method("take_damage"):
+		body.take_damage(1)
+	queue_free()
+
+func _on_timer_timeout():
+	queue_free()
